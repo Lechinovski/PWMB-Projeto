f8214d88b6d6220891ba329861ec92276f0a06c9
João Lechinovski <98363221+Lechinovski@users.noreply.github.com>
2023-09-12 17:19:56 -0700

    att

--- PWMB_ENTREGAS/PWMB-Semana#06/registar.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("form").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    var nomeInput = document.getElementById("nome").value;
-    var emailInput = document.getElementById("email").value;
-
-    window.location.href = "senhaRegistrar.html?nome=" + encodeURIComponent(nomeInput) + "&email=" + encodeURIComponent(emailInput);
-});
