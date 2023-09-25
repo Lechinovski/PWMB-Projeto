d48ac06e5244858311653c7ceea41d96cc7ab30c
João Lechinovski <98363221+Lechinovski@users.noreply.github.com>
2023-09-24 17:46:05 -0700

    att

---
+++ PWMB_ENTREGAS/PWMB-Semana#07/js/login.js
@@ -0,0 +1,8 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var emailInput = document.getElementById("email").value;
+  var senhaInput = document.getElementById("senha").value;
+
+  window.location.href = "perfil.html?email=" + encodeURIComponent(emailInput);
+});
