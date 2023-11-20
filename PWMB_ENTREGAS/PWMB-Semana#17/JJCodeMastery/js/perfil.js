045d5f427c75630c005e453713ce76225a9bd414
João Lechinovski <98363221+Lechinovski@users.noreply.github.com>
2023-11-20 12:25:27 -0800

    Add files via upload

---
+++ PWMB_ENTREGAS/PWMB-Semana#17/JJCodeMastery/js/perfil.js
@@ -0,0 +1,11 @@
+const parametros = new URLSearchParams(window.location.search);
+const nome = parametros.get('nome');
+const email = parametros.get('email');
+
+if(nome && email){
+    document.getElementById("nome").textContent = nome;
+    document.getElementById("email").textContent = email;
+    var caminhoDaImagem = 'fotoperfil.png';
+    var imagem = document.getElementById('foto');
+    imagem.src = caminhoDaImagem;
+}
